ccbaf6622fe55b92a0c54e3f268b369d1d629cb7
Sandeep-morya <saabmaurya@gmail.com>
2023-07-05 22:47:49 +0530

    jaurney-begins

---
+++ 0 - Hello-world/server.mjs
@@ -0,0 +1,24 @@
+﻿import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// The GraphQL schema
+const typeDefs = `#graphql
+    type Query {
+        greet: String
+        message:String
+    }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+	Query: {
+		greet: () => "Hello world",
+		message: () => "This is GraphQL, Used by Sandeep",
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server);
+
+console.log(`Server is ready at ${url}`);
